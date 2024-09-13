06d8dc4ce90a08599f3c1431712f7926f8d86b2d
mofq786 <mofq9786@gmail.com>
2024-09-13 21:45:41 +0530

    Security Added

--- src/main/java/com/eShopping/config/SecurityConfig.java
+++ src/main/java/com/eShopping/config/SecurityConfig.java
@@ -1,5 +1,22 @@
 package com.eShopping.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+public class SecurityConfig {
+	
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception
+	{
+		return httpSecurity.csrf(customizer -> customizer.disable())
+		.authorizeHttpRequests(request -> request.anyRequest().authenticated())
+		.formLogin(Customizer.withDefaults())
+		.httpBasic(Customizer.withDefaults())
+		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		.build();
+	}
+	
 }
